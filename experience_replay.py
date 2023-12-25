e6485069b3697ed1dd62afa329cdfcab2bf76e95
koussayinsat12 <kousai.ghaouari@insat.ucar.tn>
2023-12-25 17:12:43 +0100

    first commit

---
+++ experience_replay.py
@@ -0,0 +1,17 @@
+from collections import deque, namedtuple
+import random
+class ExperienceReplay:
+    def __init__(self, capacity, batch_size):
+        self.memory = deque(maxlen=capacity)
+        self.batch_size = batch_size
+        self.Experience = namedtuple('Experience', ['state', 'action', 'reward', 'next_state', 'done'])
+    def add_experience(self, state, action, reward, next_state, done):
+        experience = self.Experience(state, action, reward, next_state, done)
+        self.memory.append(experience)
+
+    def sample_batch(self):
+        batch = random.sample(self.memory, self.batch_size)
+        return batch
+
+    def can_provide_sample(self):
+        return len(self.memory) >= self.batch_size
